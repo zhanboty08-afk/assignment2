31373271dcb5b9d5ed6e078aec81a1391745477a
zhanboty08-afk <zhanboty08@gmail.com>
2025-12-29 10:00:48 +0500

    Update Bus.java

--- src/Bus.java
+++ src/Bus.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+public class Bus  extends Vehicle {
 
-public class Bus {
+    private int passengerCapacity;
+
+    public Bus(String model, int year, double basePrice, int passengerCapacity) {
+        super(model, year, basePrice);
+        this.passengerCapacity = passengerCapacity;
+    }
+
+    public int passengerCapacity() {
+        return passengerCapacity;
+    }
+
+    public double calculateInsuranceFee() {
+        return basePrice * 0.05;
+    }
+
+    public void performService() {
+        System.out.println("Bus " + model + " is being serviced");
+    }
+
+    public int getServiceIntervalKm() {
+        return 10000;
+    }
+
+    public String toString() {
+        return super.toString() +
+                ", Doors: " + passengerCapacity +
+                ", Type: Car";
+    }
 }
