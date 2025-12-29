3ae0fa23334a27f918ce240b0a137312b816b04b
zhanboty08-afk <zhanboty08@gmail.com>
2025-12-29 10:00:08 +0500

    Refactor Car class to extend Vehicle and add methods
    Refactored Car class to extend Vehicle and added methods for insurance fee calculation and service.

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+public class Car extends Vehicle {
 
-public class Car {
+    private int numberOfDoors;
+
+    public Car(String model, int year, double basePrice, int numberOfDoors) {
+        super(model, year, basePrice);
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public int getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public double calculateInsuranceFee() {
+        return basePrice * 0.05;
+    }
+
+    public void performService() {
+        System.out.println("Car " + model + " is being serviced");
+    }
+
+    public int getServiceIntervalKm() {
+        return 5000;
+    }
+
+    public String toString() {
+        return super.toString() +
+                ", Doors: " + numberOfDoors +
+                ", Type: Car";
+    }
 }
